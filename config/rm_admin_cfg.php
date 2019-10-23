ed0299bc8d6a5898fd0b2c4c4dd00417c50c8302
Mark Elkington <markeb14.762@gmail.com>
2019-10-23 19:02:38 +0100

    another tidy up

--- config/rm_admin_cfg.php
+++ config/rm_admin_cfg.php
@@ -1,12 +1,6 @@
 <?php
 $_SESSION['app_name'] = "admin";                        // name of application
 
-$_SESSION['db_host'] = "127.0.0.1";
-$_SESSION['db_user'] = "root";
-$_SESSION['db_pass'] = "";
-$_SESSION['db_port'] = "";
-$_SESSION['db_name'] = "pegasus";
-
 $_SESSION['sql_debug'] = false;               // set to true to turn on debugging of sql commands - otherwise false
 
 $_SESSION['daylight_saving']= array(
@@ -16,24 +10,3 @@ $_SESSION['daylight_saving']= array(
     "end_delta"   => "last sunday",
     "time_diff"   => "1"
 );
-
-//$_SESSION['webcollect']= array(
-//    "access_token"            => "986T9N5ZDSQFDBWAR4DCKOKEZS35YDFEP49KBBWMKJKXMKEHBPZOWUFC6HPZG6CS",
-//    "organisation_short_name" => "STARCROSSYC",
-//    "firstname_fld"           => "firstname",
-//    "familyname_fld"          => "lastname",
-//    "phone_fld"               => "phone",
-//    "email_fld"               => "email",
-//    "rota_fld"                => "Allocated_Duties_Club_use_only",
-//    "duty_restriction_fld"    => "Duty_Restrictions_Club_use_only",
-//    "duty_availability_fld"   => "Duty_Non_Availability_Club_use_only",
-//    "rota_code_map"           => array(
-//        "ood cruising"       => "ood_c",
-//        "safety boat driver" => "safety_d",
-//        "safety boat crew"   => "safety_c",
-//        "ood racing"         => "ood_p",
-//        "aood"               => "ood_a",
-//        "galley"             => "galley",
-//        "bar"                => "bar"
-//    )
-//);
